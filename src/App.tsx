e4fcdfd7da14e928cdafe21a1c8bc0f1a50fd533
mmmintaka <mmmintaka@gmail.com>
2024-12-05 20:57:20 +0800

    chain

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import {
   useTonAddress,
   useTonConnectModal,
@@ -13,14 +14,25 @@ function App() {
   const wallet = useTonWallet();
 
   const [amount, setAmount] = useState("0.002"); // 默认值为 0.002 TON
+  const [tonChain, setTonChain] = useState("");
+
+  useEffect(() => {
+    // 获取 URL 查询参数
+    const urlParams = new URLSearchParams(window.location.search);
+    const tonChainParam = urlParams.get("tonChain");
+    if (tonChainParam) {
+      setTonChain(tonChainParam);
+      console.log("接收到的 tonChain:", tonChainParam);
+    }
+  }, []);
 
   const sendTransaction = async () => {
     const transaction = {
       validUntil: Math.floor(Date.now() / 1000) + 60, // 60 sec
       messages: [
         {
-          address: "UQCQ0pohji80O14DRaPl9Z2aCZxDY5405t6SF6EVzz4gmu9q",
-          amount: 0.002 * 10 ** 9,
+          address: tonChain, // 使用接收到的 tonChain
+          amount: Number(amount) * 10 ** 9,
         },
       ],
     };
@@ -33,44 +45,42 @@ function App() {
       console.log(123, error);
     }
   };
+
   return (
     <>
-      <div className=" flex-col gap-4 flex items-center justify-end">
-        {/* <TonConnectButton /> */}
+      <div className="flex-col gap-4 flex items-center justify-end">
         {userFriendlyAddress ? (
           <div onClick={() => tonConnectUI.disconnect()}>disconnect</div>
         ) : (
           <div onClick={() => tonConnectUI.openModal()}>Open Modal</div>
         )}
       </div>
-      <div className=" flex flex-col mt-4 gap-2 border border-solid border-red-400 p-3 rounded-[20px]">
-        <div> model state : {state?.status}</div>
-        <div className="">address : {userFriendlyAddress || "none"}</div>
+
+      <div>传递进来的地址： {tonChain}</div>
+
+      <div className="flex flex-col mt-4 gap-2 border border-solid border-red-400 p-3 rounded-[20px]">
+        <div>model state: {state?.status}</div>
+        <div>address: {userFriendlyAddress || "none"}</div>
       </div>
 
-      <div className=" flex flex-col mt-4 mb-5 gap-2 border border-solid border-red-400 p-3 rounded-[20px]">
+      <div className="flex flex-col mt-4 mb-5 gap-2 border border-solid border-red-400 p-3 rounded-[20px]">
         current wallet:
         {wallet && (
           <>
             <span>Device: {wallet.device.appName}</span>
             <span>Connected via: {wallet.provider}</span>
-            {/* 
-  <div>
-  Connected wallet info:
-  {wallet?.name} < img className=" w-4" src={wallet?.imageUrl} />
-  </div> */}
           </>
         )}
       </div>
 
-      <div className=" flex gap-4 border border-solid border-red-400 p-3 rounded-[20px]">
+      <div className="flex gap-4 border border-solid border-red-400 p-3 rounded-[20px]">
         <div onClick={sendTransaction}>send transaction to my address</div>
         <input
-          className=" border border-solid border-blue-700"
+          className="border border-solid border-blue-700"
           type="text"
-          placeholder=" 转账ton金额"
-          value={amount} // 绑定输入框的值
-          onChange={(e) => setAmount(e.target.value)} // 更新状态
+          placeholder="转账ton金额"
+          value={amount}
+          onChange={(e) => setAmount(e.target.value)}
         />
       </div>
     </>
